b3e43d872e82506658b71abd7ff7b0d3aaba34e9
Leonce Nshuti <LNshuti@users.noreply.github.com>
2024-07-11 12:47:30 -0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,21 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:posthog_flutter/posthog.dart';
-import 'config.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
-void main() {
-  Posthog().init(Config.posthogApiKey, host: Config.posthogHost);
-  runApp(MyApp());
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class MyApp extends StatelessWidget {
+class _MyHomePageState extends State<MyHomePage> {
+  List<charts.Series> seriesList = [];
+  bool loading = false;
+
+  void _fetchData(String ticker) async {
+    setState(() {
+      loading = true;
+    });
+    final data = await fetchETFData(ticker);
+    setState(() {
+      // Update seriesList with the new data
+      loading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ETF Tracker',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ETF Tracker'),
       ),
-      home: MyHomePage(),
+      body: loading
+          ? Center(child: CircularProgressIndicator())
+          : Column(
+              children: <Widget>[
+                Expanded(
+                  child: charts.TimeSeriesChart(
+                    seriesList,
+                    animate: true,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () => _fetchData('IVV'),
+                  child: Text('Fetch ETF Data'),
+                ),
+              ],
+            ),
     );
   }
 }
